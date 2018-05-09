504bf255972902699bbc3af5926a0260cf3d5c7a
Yogesh Kumar <balanyogesh@gmail.com>
2018-05-09 20:20:09 +0530

    removed API keys

--- app/src/main/java/in/yogesh/searchx/app/SearchConstants.java
+++ app/src/main/java/in/yogesh/searchx/app/SearchConstants.java
@@ -7,8 +7,8 @@ public class SearchConstants {
 
     public static final String APP_NAME = "SearchX";
 
-    public static final String CUSTOM_SEARCH_API_KEY = "AIzaSyDQ7ouFH_Bxv9TXV5-77FvDlir2g08oeW4";
-    public static final String CUSTOM_SEARCH_ENGINE_KEY = "011346167944233595302:qmesrn8ddqs";
+    public static final String CUSTOM_SEARCH_API_KEY = ""; // <-- Add you API key here
+    public static final String CUSTOM_SEARCH_ENGINE_KEY = ""; // <-- Add your custom search engine ID here
     public static final String CUSTOM_SEARCH_TYPE = "image";
 
 }
